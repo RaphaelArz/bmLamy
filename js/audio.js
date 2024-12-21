33bff277ddd7b5cc92c2af550cde233d44c1c160
Raphael <raphaelarzoin173@gmail.com>
2024-12-21 22:31:17 +0100

    vEtoile

--- js/audio.js
+++ js/audio.js
@@ -1,4 +1,14 @@
 function playMusic() {
     var audio = document.getElementById("myAudio");
+    audio.volume = 0; // Commence avec le volume à 0
     audio.play();
+
+    // Augmente progressivement le volume
+    var fadeInInterval = setInterval(function () {
+        if (audio.volume < 1) {
+            audio.volume = Math.min(audio.volume + 0.005, 1); // Incrémente le volume
+        } else {
+            clearInterval(fadeInInterval); // Arrête l'intervalle lorsque le volume atteint 1
+        }
+    }, 200); // Incrémente toutes les 200 ms
 }
