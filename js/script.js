eaaa706f4e84c2d08a1c236fad7e4a8c9471fcf0
Raphael <raphaelarzoin173@gmail.com>
2024-12-21 22:25:12 +0100

    vEtoile

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-// Ajoutez la classe no-scroll par défaut lorsque la page se charge
+// Ajoute la classe no-scroll par défaut lorsque la page se charge
 document.body.classList.add("no-scroll");
 
-function hideOverlay() {
-  var overlay = document.getElementById('overlay');
-  overlay.classList.add('hidden'); // Ajoute la classe hidden pour masquer l'overlay
-  document.body.classList.remove("no-scroll"); // Retire la classe no-scroll pour permettre le défilement
+function startCountdown() {
+  const button = document.getElementById("invitationButton");
+  const countdown = document.getElementById("countdown");
+  const overlay = document.getElementById("overlay");
+
+  // Cache le bouton
+  button.style.display = "none";
+
+  // Affiche le compte à rebours
+  countdown.style.display = "block";
+
+  let timeLeft = 48;
+
+  const timer = setInterval(() => {
+    timeLeft--;
+    countdown.textContent = timeLeft;
+
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      countdown.style.display = "none"; // Cache le compte à rebours
+      overlay.classList.add("hidden"); // Masque l'overlay
+      document.body.classList.remove("no-scroll"); // Permet le défilement
+    }
+  }, 1000); // Décrément toutes les secondes
 }
